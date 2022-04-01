144f4b80ca804fabaaf4f5edefa02a6917c0ceb0
raminelnunez <Raminelnc23@gmail.com>
2022-04-01 14:02:07 -0500

    Add project class

--- AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Models/Project.cs
+++ AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Models/Project.cs
@@ -2,5 +2,12 @@
 {
     public class Project
     {
+        public bool isCompleted { get; set; }
+        public enum Priority { Low, Medium, High}
+        public int CompletionPercentage { get; set; }
+        public DateTime Deadline { get; set; }
+        public ProjectManager ProjectManager { get; set; }
+        public List<ProjectTask> Tasks { get; set; }
+        public List<Developer> Developers { get; set; }
     }
 }
